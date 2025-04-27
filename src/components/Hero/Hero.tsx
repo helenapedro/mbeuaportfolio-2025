755f4cfb37397844ccdf501e94b087f646f56cb7
Helena Pedro <mbeua94@gmail.com>
2025-04-26 23:23:33 -0500

    new commit

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,8 +5,8 @@ import { StatusBadge } from './StatusBadge';
 import { SocialLinks } from './SocialLinks';
 
 //const BASE_URL = "https://pedropublicfiles.s3.us-east-2.amazonaws.com/portfolio";
-const ADDRESS = "Talatona, AO";
-const TIMEZONE = "4 hrs overlap (PST/EST)";
+const ADDRESS = "Fairfield, IA";
+const TIMEZONE = "Central Daylight Time (GMT-5)";
 const JOB_TITLE = "Software Development Engineer";
 
 export function Hero() {
