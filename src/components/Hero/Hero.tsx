c0032ff413509084a68173b40570750420cfd893
Helena Pedro <mbeua94@gmail.com>
2025-04-27 00:21:51 -0500

    new commit

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,7 +7,7 @@ import { SocialLinks } from './SocialLinks';
 //const BASE_URL = "https://pedropublicfiles.s3.us-east-2.amazonaws.com/portfolio";
 const ADDRESS = "Fairfield, IA";
 const TIMEZONE = "Central Daylight Time (GMT-5)";
-const JOB_TITLE = "Software Development Engineer";
+const JOB_TITLE = "SDE / Architect: Microservices, Cloud & Distributed Systems";
 
 export function Hero() {
   return (
